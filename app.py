209c7ff6e05d849a0be15deec55579bb7815af33
Alex <nypels.alexandre@outlook.com>
2022-02-09 13:31:48 +0000

    Fix deployement issue

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
